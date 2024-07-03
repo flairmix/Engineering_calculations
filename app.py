18121ee604379cc28129bb556503232c512a6ba9
flairmix <d9067246810@gmail.com>
2024-07-03 18:58:00 +0300

    start_with_rain_calc

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ from litestar import Litestar, get
 from users_managment.controller import UserController
 from calculations.Water_general.controller import WaterCalcController
 from calculations.Sheveleva_tables.controller import ShevelevController
-
-@get("/")
-async def index() -> str:
-    return "Hello, world!"
-
-
-@get("/books/{book_id:int}")
-async def get_book(book_id: int) -> dict[str, int]:
-    return {"book_id": book_id}
+from calculations.Rain_calc.controller import RainCalcController
 
 
 app = Litestar(route_handlers=[UserController, 
                                WaterCalcController, 
-                               ShevelevController], 
+                               ShevelevController, 
+                               RainCalcController], 
                                debug=True)
