3a91a8607d86d29f1cf9c657ee49fd42867f3f2e
flairmix <d9067246810@gmail.com>
2024-07-24 00:47:54 +0300

    added receiving rain parameters city that inputed

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from litestar import Litestar, get
 from users_managment.controller import UserController
 from calculations.Water_general.controller import WaterCalcController
 from calculations.Sheveleva_tables.controller import ShevelevController
-from calculations.Rain_calc.controller import RainCalcController
+from calculations.Rain_calc.controller import RainController
 
 
 app = Litestar(route_handlers=[UserController, 
                                WaterCalcController, 
                                ShevelevController, 
-                               RainCalcController], 
+                               RainController], 
                                debug=True)
